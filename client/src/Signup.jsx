5d7f12886adc9bbf57d52cf8a23c7a123de2a92d
Krishna Tripathy <98588847+KrishnaTripathy@users.noreply.github.com>
2024-01-18 14:08:18 +0530

    Update Signup.jsx

--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -11,7 +11,7 @@ function Signup() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('http://localhost:3001/register', { name, email, password })
+    axios.post('https://secret-message-5ie3.onrender.com/register', { name, email, password })
       .then(result => {
         console.log(result);
         navigate('/login');

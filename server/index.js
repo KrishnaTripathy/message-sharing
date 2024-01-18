4df83032ef49294c4b083125820e245a0756f457
Krishna Tripathy <98588847+KrishnaTripathy@users.noreply.github.com>
2024-01-18 17:02:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(session({
 }));
 
 // Handle MongoDB connection properly
-mongoose.connect("mongodb://127.0.0.1:27017/employee")
+mongoose.connect("mongodb+srv://krishnatripathy:Krishna%40250301@krishna.hcdyjma.mongodb.net/")
   .then(() => {
     console.log('Connected to MongoDB');
   })
